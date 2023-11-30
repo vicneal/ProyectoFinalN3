bbda2ad13332df0aa31327152e28dd8c10ff55b6
Neal <victor.negocios.l.17@gmail.com>
2023-11-30 14:20:37 -0500

    ..

--- db_exportada/universidad_alumnos.sql
+++ db_exportada/universidad_alumnos.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `universidad` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `universidad`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: universidad
@@ -52,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-30 12:58:53
+-- Dump completed on 2023-11-30 14:18:58
